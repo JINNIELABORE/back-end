f783fc5d88d77e99d8d6eedb76320caa052e368c
Gustavo <santosguhenrique@gmail.com>
2024-09-05 11:59:05 -0300

    Database Atualizado com função Premium

--- database/db_jinni.sql
+++ database/db_jinni.sql
@@ -7,9 +7,9 @@ id int not null auto_increment primary key,
 nome_cliente varchar(50) not null,
 cnpj_cliente bigint(14) not null,
 email_cliente varchar(255) not null,
-senha_cliente varchar(80)not null
+senha_cliente varchar(80)not null,
+is_premium boolean not null default false
 );
-
 
 create table cadastro_freelancer(
 id int not null auto_increment primary key,
@@ -17,5 +17,7 @@ nome_freelancer varchar(50) not null,
 data_nascimento date not null,
 cpf_freelancer bigint(12) not null,
 email_freelancer varchar(255) not null,
-senha_freelancer varchar(80) not null
+senha_freelancer varchar(80) not null,
+is_premium boolean not null default false
 );
+

5cb0e0f3e02819ceeec2d32b33013d641b443259
JINNI <elaborejinni@gmail.com>
2024-10-17 11:57:45 -0300

    criação das tabelas no banco

--- database/db_jinni.sql
+++ database/db_jinni.sql
@@ -50,10 +50,10 @@ create table freelancer_categoria (
   id int not null auto_increment primary key,
   id_freelancer int not null,
   id_categoria int not null,
-  
+ 
   foreign key (id_freelancer) references cadastro_freelancer(id),
   foreign key (id_categoria) references categorias(id),
-  
+ 
   unique key (id),
   unique index (id)
 );
@@ -63,10 +63,47 @@ create table freelancer_habilidade (
   id int not null auto_increment primary key,
   id_freelancer int not null,
   id_habilidade int not null,
-  
+ 
   foreign key (id_freelancer) references cadastro_freelancer(id),
   foreign key ( id_habilidade) references habilidades(id),
-  
+ 
   unique key (id),
   unique index (id)
+);
+
+create table nivel_experiencia(
+id int not null auto_increment primary key,
+nivel_experiencia varchar(20)
+);
+
+create table publicacao_projetos(
+id int not null auto_increment primary key ,
+nome_projeto varchar(50),
+descricao_projeto varchar(150),
+orcamento double not null,
+id_nivel_experiencia int not null,
+
+id_categoria int not null,
+id_habilidade int not null,
+
+foreign key (id_categoria) references categorias(id),
+foreign key (id_habilidade) references habilidades(id),
+foreign key (id_nivel_experiencia) references nivel_experiencia(id)
+);
+
+
+create table descricao_perfil(
+id int not null auto_increment primary key,
+descricao varchar(200)
+);
+
+create table foto_perfil(
+id int not null auto_increment primary key,
+foto_perfil text
+
+);
+
+create table portfolio(
+id int not null auto_increment primary key,
+arquivo text
 );
